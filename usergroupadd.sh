b028b3fa5b89fd12df6008b1a82a48b02fdecdcb
thedarklord6609 <hahn.nathan.a@gmail.com>
2024-10-04 02:06:26 -0400

    Update usergroupadd.sh

--- usergroupadd.sh
+++ usergroupadd.sh
@@ -3,8 +3,8 @@
 # Uses admin permissions to create the docker group and add the user to it
 
 username="$1"
-
-sudo groupadd docker
+sudo -i
+groupadd docker
 chgrp docker /var/run/docker.sock
 newgrp docker
 usermod -aG docker "$username"
