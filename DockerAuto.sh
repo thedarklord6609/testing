54ffea61397246f82e2c49756846775863e5f77e
thedarklord6609 <hahn.nathan.a@gmail.com>
2024-10-04 02:22:01 -0400

    Create DockerAuto.sh

---
+++ DockerAuto.sh
@@ -0,0 +1,16 @@
+#!/bin.bash
+
+username="$1"
+
+sudo apt update -y
+sudo -i
+addgroup docker
+chgrp docker /var/run/docker.sock
+usermod -aG docker "$username"
+newgrp docker
+
+mkdir minecraft
+cd minecraft
+wget "https://raw.githubusercontent.com/itzg/docker-minecraft-server/refs/heads/master/docker-compose.yml"
+
+docker compose up -d
