c1272b54f38a5ab097a1a6f391a986077acd17ab
thedarklord6609 <hahn.nathan.a@gmail.com>
2024-10-04 13:08:26 -0400

    Update Forge.sh

--- Forge.sh
+++ Forge.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 version="$1"
 
-if "$1"="1.20.1"
+if "$1"="1.21.1"
+
+if "$1"="1.21"
+
+# alternatively look into direct website connections, $1 looks for version number, goes to https, and downloads directly 
