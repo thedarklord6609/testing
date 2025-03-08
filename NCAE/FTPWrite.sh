826bca2e24e58cdfb978673a76346fe93e7d8c98
thedarklord6609 <hahn.nathan.a@gmail.com>
2025-03-08 03:03:04 -0500

    Delete NCAE/FTPWrite.sh

--- NCAE/FTPWrite.sh
+++
@@ -1,7 +0,0 @@
-# List of FTP scoring users
-users=("camille_jenatzy" "gaston_chasseloup" "leon_serpollet" "william_vanderbilt" "henri_fournier" "maurice_augieres" "arthur_duray" "henry_ford" "louis_rigolly" "pierre_caters" "paul_baras" "victor_hemery" "fred_marriott" "lydston_hornsted" "kenelm_guinness" "rene_thomas" "ernest_eldridge" "malcolm_campbell" "ray_keech" "john_cobb" "dorothy_levitt" "paula_murphy" "betty_skelton" "rachel_kushner" "kitty_oneil" "jessi_combs" "andy_green")
-
-# Loop to apply write permission for each user
-for user in "${users[@]}"; do
-  sudo setfacl -m u:$user:rw /mnt/files/*
-done
