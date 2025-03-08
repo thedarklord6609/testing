343be01360cbddbece4592f12f2de26df5b4c0d2
thedarklord6609 <hahn.nathan.a@gmail.com>
2025-03-08 10:18:56 -0500

    Create add.sh

---
+++ NCAE/add.sh
@@ -0,0 +1,6 @@
+mv -- "iron_cross.data" "3_point_molly.data" "dark_side.data" "come_dont_come.data" \
+      "odds.data" ".house_secrets.data" "pass_line.data" "risky_roller.data" \
+      "covered_call.data" "married_put.data" "bull_call.data" "protective_collar.data" \
+      "long_straddle.data" "long_strangle.data" "long_call_butterfly.data" \
+      "iron_condor.data" "iron_butterfly.data" "short_put.data" \
+      "data_dump_1.bin" "data_dump_2.bin" "data_dump_3.bin" "datadump.bin" /mnt/files
